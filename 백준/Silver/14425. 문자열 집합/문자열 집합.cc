fd179be2fcfe016a58467fc8dbfcbb083ac525cb
Hayeon-Lee <57438406+Hayeon-Lee@users.noreply.github.com>
2022-09-03 23:35:56 +0900

    [Silver III] Title: 문자열 집합, Time: 368 ms, Memory: 7704 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.cc
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cc
@@ -1,33 +1,28 @@
-#include <set>
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <algorithm>
-#include <vector>
-
+#include <set>
 using namespace std;
 
 int main()
 {
-    int n;
-    int m;
-    cin >> n;
-    cin >> m;
-    set<string> arr1;
-    string arr2;
-    string u;
-    int count = 0;
+	int N, M, count = 0;
+	scanf("%d %d", &N, &M);
 
-    for (int a = 0; a < n; a++)
-    {
-        cin >> u;
-        arr1.insert(u);
-    }
-    for (int a = 0; a < m; a++)
-    {
-        cin >> arr2;
-        if (arr1.find(arr2) != arr1.end())
-        {
-            count++;
-        }
-    }
-    cout << count;
+	set<string> strings;
+	string tmp;
+
+	for (int i = 0; i < N; i++) {
+		cin >> tmp;
+		strings.insert(tmp);
+	}
+
+	set<string>::iterator it;
+	for (int i = 0; i < M; i++) {
+		cin >> tmp;
+		//it = strings.find(tmp);
+
+		if (strings.find(tmp) != strings.end()) count++;
+	}
+
+	printf("%d", count);
 }
