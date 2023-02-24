489f65e40bdd6b4e5e818477be1593a80ff24e6b
이하연 <57438406+Hayeon-Lee@users.noreply.github.com>
2023-02-24 22:07:44 +0900

    [Silver III] Title: 안테나, Time: 44 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.cc
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int N;
+int* home;
+
+bool compare(pair<int, int>a, pair<int, int> b) {
+    if (a.first == b.first) return a.second < b.second;
+    else return a.first < b.first;
+}
+
+void input() {
+    scanf("%d", &N);
+    home = new int[N];
+
+    for (int i = 0; i < N; i++) {
+        int n;
+        scanf("%d", &n);
+        home[i] = n;
+    }
+}
+
+void solution() {
+    sort(home, home + N); //집 정렬
+    printf("%d", home[(N - 1) / 2]);
+   }
+
+int main() {
+    input();
+    solution();
+}
+
+/*
+4
+5 1 7 9
+*/
