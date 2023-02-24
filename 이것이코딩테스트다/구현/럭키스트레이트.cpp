d2f84e9e4b2a488784d82d3339c3aed2391aa440
이하연 <57438406+Hayeon-Lee@users.noreply.github.com>
2023-02-24 16:43:53 +0900

    Create 럭키스트레이트.cpp

---
+++ 이것이코딩테스트다/구현/럭키스트레이트.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+string userInput;
+int inputsize = 0, mid = 0, inputLeft = 0, inputRight = 0;
+
+void input() {
+    cin >> userInput;
+    mid = (userInput.size() / 2);
+    inputsize = userInput.size();
+}
+
+void solution() {
+    for (int i = 0; ai < mid; i++) inputLeft += (userInput[i] - '0');
+    for(int i=mid; i<inputsize; i++) inputRight+= (userInput[i] - '0');
+    
+    if (inputLeft == inputRight) printf("LUCKY");
+    else printf("READY");
+}
+
+int main() {
+    input();
+    solution();
+}
