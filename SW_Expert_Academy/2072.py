c9b40e8a18da35c3cadc03f67b978ec68bb87d23
이하연 <57438406+Hayeon-Lee@users.noreply.github.com>
2023-10-18 13:28:18 +0900

    Create 2072.py

---
+++ SW_Expert_Academy/2072.py
@@ -0,0 +1,11 @@
+T = int(input())
+ 
+for i in range(T):
+    inputs = list(map(int, input().split()))
+ 
+    answer = 0
+    for n in inputs:
+        if n%2 != 0:
+            answer += n
+     
+    print("#%d %d" %(i+1, answer))
