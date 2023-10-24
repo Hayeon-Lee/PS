25f52a5e386d10ad86a92431313ef8a7787c3888
이하연 <57438406+Hayeon-Lee@users.noreply.github.com>
2023-10-24 19:15:16 +0900

    [Silver I] Title: 숨바꼭질, Time: 436 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,42 +1,26 @@
-import sys
+import sys 
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
-MAX_NUM = 100001
+MAX = 10**5 + 1
+dist = [0 for _ in range(MAX)]
+queue = list()
 
 def solution():
     global queue
 
-    while len(queue)!=0:
+    queue.append(N)
 
-        now = queue[0]
-        queue.pop(0)
-
+    while queue:
+        now = queue.pop(0)
         if now == K:
-            return
-        
-        if now + 1 < MAX_NUM and not visit[now+1]:
-            visit[now+1] = 1
-            queue.append(now+1)
-            board[now+1] = board[now]+1
-        
-        if now - 1 >= 0 and not visit[now-1]:
-            visit[now-1] = 1
-            queue.append(now-1)
-            board[now-1] = board[now]+1
-        
-        if now*2 < MAX_NUM and not visit[now*2]:
-            visit[now*2] = 1
-            queue.append(now*2)
-            board[now*2] = board[now]+1
+            print(dist[K])
+            break
 
+        for nx in (now+1, now-1, now*2):
+            if 0 <= nx < MAX and not dist[nx]:
+                dist[nx] = dist[now]+1
+                queue.append(nx)
 
 N, K = map(int, input().split())
-board = [0 for _ in range(MAX_NUM)]
-visit = [0 for _ in range(MAX_NUM)]
-queue = list()
 
-board[N] = 0
-visit[N] = 1
-queue.append(N)
 solution()
-print(board[K])
+
