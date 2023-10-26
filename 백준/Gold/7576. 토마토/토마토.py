253c28a2095aebec08415855bea1b380ff05db0a
이하연 <57438406+Hayeon-Lee@users.noreply.github.com>
2023-10-26 21:33:46 +0900

    [Gold V] Title: 토마토, Time: 1544 ms, Memory: 106800 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,44 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+#상하좌우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def solution():
+    global tomato_box, deque
+        
+    while (deque):
+        h, w = deque.popleft()
+
+        for i in range(4):
+            nx = h + dx[i]
+            ny = w + dy[i]
+
+            if 0 <= nx < height and 0 <= ny <width and tomato_box[nx][ny] == 0:
+                deque.append([nx, ny])
+                tomato_box[nx][ny] = tomato_box[h][w]+1
+
+width, height = map(int, input().split())
+
+tomato_box = [list(map(int, input().split())) for _ in range(height)]
+deque= deque()
+max_day = 0
+
+for h in range(height):
+    for w in range(width):
+        if tomato_box[h][w] == 1:
+            deque.append([h, w])
+
+solution()
+
+for h in tomato_box:
+    for w in h:
+        if w == 0:
+            print(-1)
+            exit()
+    
+    max_day = max(max_day, max(h))
+
+print(max_day-1)
