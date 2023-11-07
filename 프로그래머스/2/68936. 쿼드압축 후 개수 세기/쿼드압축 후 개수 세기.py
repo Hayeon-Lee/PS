b23956316879fa1be57b6b98add4616a6304bc13
이하연 <57438406+Hayeon-Lee@users.noreply.github.com>
2023-11-07 16:47:18 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 61.37 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -1,21 +1,23 @@
-def compress(w, h, l, answer, arr):
+def compress(w, h, l,arr):
     value = arr[w][h]
     for i in range(w, w+l):
         for j in range(h, h+l):
             if arr[i][j] != value:
                 l = l//2
-                compress(w,h,l,answer,arr)
-                compress(w,h+l,l,answer,arr)
-                compress(w+l,h,l,answer,arr)
-                compress(w+l,h+l,l,answer,arr)
+                compress(w,h,l,arr)
+                compress(w,h+l,l,arr)
+                compress(w+l,h,l,arr)
+                compress(w+l,h+l,l,arr)
                 return
     answer[value] += 1
                 
 
 def solution(arr):
+    global answer
     length = len(arr)
     answer = [0, 0]
-    compress(0, 0, length, answer, arr)
+    
+    compress(0, 0, length, arr)
     
     return answer
 
