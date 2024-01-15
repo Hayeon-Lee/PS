257cdf32936609fdb516bf0443e459e1d942b825
이하연 <papepopepe@naver.com>
2024-01-15 14:42:56 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+stack = []
+for _ in range(N):
+    order = input().split()
+
+    if order[0] == 'push':
+        stack.append(order[1])
+
+    if order[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
+
+    if order[0] == 'size':
+        print(len(stack))
+
+    if order[0] == 'empty':
+        if stack: print(0)
+        else: print(1)
+
+    if order[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
