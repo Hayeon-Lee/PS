fff2a8bc0f95a9cc49245c5352dbc25677c00431
이하연 <papepopepe@naver.com>
2024-01-20 20:33:35 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1852 ms, Memory: 98816 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,35 +1,36 @@
-from collections import deque
 import sys
-f = sys.stdin.readline
+from collections import deque
+input = sys.stdin.readline
 
-n, m, k, x = map(int, f().split())
+n, m, k, x = map(int, input().split())
 graph = [[] for _ in range(n+1)]
-distance = [0] * (n+1)
-visited = [False] * (n+1)
+visit = [0] * (n+1)
+queue = deque()
+
+def bfs():
+
+    global queue, visit
+
+    while(queue):
+        start = queue.popleft()
+        for v in sorted(graph[start]):
+            if visit[v] == 0:
+                visit[v] = (visit[start] + 1)
+                queue.append(v)
 
 for _ in range(m):
-    a, b = map(int, f().split())
+    a, b = map(int, input().split())
     graph[a].append(b)
 
-def bfs(start):
-    answer = []
-    q = deque([start])
-    visited[start] = True
-    distance[start] = 0
-    while q:
-        now = q.popleft()
-        for i in graph[now]:
-            if not visited[i]:
-                visited[i] = True
-                q.append(i)
-                distance[i] = distance[now] + 1
-                if distance[i] == k:
-                    answer.append(i)
-    if len(answer) == 0:
-        print(-1)
-    else:
-        answer.sort()
-        for i in answer:
-            print(i, end='\n')
+queue.append(x)
+visit[x] = 1
+bfs()
 
-bfs(x)
+answer = []
+for v in range(1, n+1):
+    if visit[v] == k+1:
+        answer.append(v)
+
+if len(answer) == 0: print(-1)
+else: 
+    for a in sorted(answer): print(a)
