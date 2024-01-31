8a6480f97b414596fc24eb14959c0fd500695f1d
이하연 <papepopepe@naver.com>
2024-01-31 18:50:49 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,14 +1,39 @@
-def solution(n, costs):
-    answer = 0
-        
-    costs.sort(key=lambda x:x[2])
-    link = set([costs[0][0]])
+def getParent(parent, x):
+    if parent[x] == x:
+        return x
+    return getParent(parent, parent[x])
+
+def unionParent(parent, a, b):
+    a = getParent(parent, a)
+    b = getParent(parent, b)
     
-    while len(link)!=n:
-        for v in costs:
-            if v[0] in link and v[1] in link: continue
-            if v[0] in link or v[1] in link:
-                link.update([v[0], v[1]])
-                answer += v[2]
-                break
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+def sameParent(parent, a, b):
+    a = getParent(parent, a)
+    b = getParent(parent, b)
+    
+    if a == b:
+        return True
+    else:
+        return False
+
+def solution(n, costs):
+    parent = [i for i in range(n)]
+    answer = 0
+    count = 0
+    
+    costs = sorted(costs, key=lambda x: x[2])
+    length = len(costs)
+    
+    for cost in costs:
+        s, e, c = cost
+        
+        if sameParent(parent, s, e) == False:
+            unionParent(parent, s, e)
+            answer += c
+            
     return answer
