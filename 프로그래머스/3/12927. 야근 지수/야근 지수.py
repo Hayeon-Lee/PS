57b5c88fe71fee6efe25a809880ee5d669299168
이하연 <papepopepe@naver.com>
2024-02-01 17:44:22 +0900

    [level 3] Title: 야근 지수, Time: 1042.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -2,21 +2,21 @@ import heapq
 
 def solution(n, works):
     heap = []
-    
+
     for work in works:
         heapq.heappush(heap, work*-1)
-    
+
     while heap:
         if heap[0] == 0 or n == 0:
             break
-        
+
         now = abs(heapq.heappop(heap))
         now -= 1
         n -= 1
-        
+
         if now != 0:
             heapq.heappush(heap, now*-1)
-    
+
     if not heap:
         return 0
     else:
@@ -24,7 +24,3 @@ def solution(n, works):
         for h in heap:
             answer += h**2
         return answer
-'''
-제곱을 하면 숫자가 커지기 때문에 가능한 많은 일을 적게 남겨야 한다
-먼저 처리해야 할 일은 1과 0이 아닌 일들부터
-'''
