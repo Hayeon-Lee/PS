0874c6139e0557be5be4e35c1864a622f13eba77
이하연 <papepopepe@naver.com>
2024-02-01 16:08:30 +0900

    [level 2] Title: 배달, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,38 +1,38 @@
-import heapq
 import sys
+import heapq
 
 def dijkstra(distances, graph):
     heap = []
-    heapq.heappush(heap, (0, 1))
+    heapq.heappush(heap, (0, 1)) #초기값 
     distances[1] = 0
     
     while heap:
-        dist, now = heapq.heappop(heap)
-        if distances[now] < dist:
-            continue
-        for item in graph[now]:
-            c, e = item
-            cost = dist + c
+        now_cost, now_dest  = heapq.heappop(heap)
+        
+        for item in graph[now_dest]:
+            next_cost, next_dest = item
             
-            if cost < distances[e]:
-                distances[e] = cost
-                heapq.heappush(heap, (cost, e))
+            new_cost = (now_cost + next_cost) #거쳐갔을 때 비용 
+            
+            if new_cost < distances[next_dest]:
+                distances[next_dest] = new_cost
+                heapq.heappush(heap, (new_cost, next_dest))
+            
 
 def solution(N, road, K):
-    
     distances = [sys.maxsize] * (N+1)
-    graph = [[] * (N+1) for _ in range(N+1)]
-
+    graph = [[] for _ in range(N+1)]
+    
     for r in road:
         s, e, c = r
-        graph[s].append((c,e))
-        graph[e].append((c,s))
-    
+        graph[s].append((c, e))
+        graph[e].append((c, s))
+        
     dijkstra(distances, graph)
-    
+        
     answer = 0
-    for distance in distances[1:]:
+    for distance in distances:
         if distance <= K:
             answer += 1
-    
+        
     return answer
