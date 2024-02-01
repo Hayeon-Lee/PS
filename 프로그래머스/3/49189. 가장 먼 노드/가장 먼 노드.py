e735164d5d89e873c94cf5b5c7c420d4c8576e11
이하연 <papepopepe@naver.com>
2024-02-01 18:11:41 +0900

    [level 3] Title: 가장 먼 노드, Time: 30.36 ms, Memory: 20.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+def bfs(graph, visit, distance):
+    queue = deque([1])
+    visit[1] = True
+    
+    while queue:
+        now = queue.popleft()
+        
+        for node in sorted(graph[now]):
+            if visit[node] == False :
+                distance[node] = distance[now]+1
+                queue.append(node)
+                visit[node] = True
+
+def solution(n, edge):
+    graph = [[] for _ in range(n+1)]
+    visit = [False] * (n+1)
+    distance = [0] * (n+1)
+    
+    for e in edge:
+        start, end = e
+        graph[start].append(end)
+        graph[end].append(start)
+    
+    bfs(graph, visit, distance)
+    
+    maximum = max(distance)
+    distance = sorted(distance, reverse=True)
+    answer = 0
+    
+    for d in distance:
+        if d == maximum:
+            answer += 1
+    
+    return answer
