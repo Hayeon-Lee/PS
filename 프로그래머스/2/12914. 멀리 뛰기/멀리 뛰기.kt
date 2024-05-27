33f1e0582fa261c841702cfed786e69fb42029e7
이하연 <papepopepe@naver.com>
2024-05-27 14:01:59 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.kt
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.kt
@@ -1,14 +1,17 @@
 class Solution {
-
+    
     fun solution(n: Int): Long {
-        var dp = LongArray(n+1)
-        dp[0]=1
-        dp[1]=1
-        if(n<2) return dp[n]
-        for(i in 2 ..n){
-            dp[i] = (dp[i-1]+dp[i-2])%123_456_7
+        var answerArr = LongArray(n+1)
+        
+        answerArr[0] = 1
+        answerArr[1] = 1
+        
+        if (n<2) return answerArr[n]%123_456_7
+        
+        for (i: Int in 2..n) {
+            answerArr[i] = (answerArr[i-1]+answerArr[i-2])%123_456_7
         }
-
-        return dp[n]
+        
+        return answerArr[n]
     }
 }
