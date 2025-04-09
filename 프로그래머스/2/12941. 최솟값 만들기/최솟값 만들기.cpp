b50adc9bc95706d6d3184eb465a1be635380caa9
이하연 <papepopepe@naver.com>
2025-04-09 21:28:04 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.11 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(int x, int y) {
+    return x > y;
+}
+
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), compare);
+    
+    for (int i=0; i < A.size(); i++) {
+        answer += (A[i] * B[i]);
+    }
+
+    return answer;
+}
