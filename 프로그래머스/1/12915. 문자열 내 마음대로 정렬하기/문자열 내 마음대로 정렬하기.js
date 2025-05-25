62f6f7b14d66a8447f89d9032100c5e16001fa4e
이하연 <papepopepe@naver.com>
2025-05-25 09:53:58 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 768.36 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,12 @@
+function solution(strings, n) {
+    
+    strings.sort((a,b) => {
+            if (a[n] === b[n]) {
+                return a.localeCompare(b);
+            } else {
+                return a[n].localeCompare(b[n]);
+            }
+        });
+    
+    return strings;
+}
